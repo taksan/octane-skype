547b9c88cdba5c960a202385c55032a7da9d66a0
takeuchi <g.takeuchi@gmail.com>
2016-09-06 07:20:30 -0300

    Updating script to build for other ubuntu series

--- perform-linux-release.sh
+++ perform-linux-release.sh
@@ -2,6 +2,25 @@
 
 set -e 
 
+function wait_for()
+{
+    log "Counting down..." 
+    local secs=$1
+    while [ $secs -gt 0 ]; do
+        echo -ne "$secs\033[0K\r" >&2
+        # if any key is pressed, stop counting and return; or else decrement counter
+        read -t 1 -n 1 -s && return || : $((secs--))
+    done
+}
+
+DIST=$1
+if [[ -z $DIST ]]; then
+	echo "Series not provided. Series can be: trusty vivid wily xenial"
+	DIST=$(echo $(lsb_release -c|cut -d: -f2))
+	echo "Using $DIST in 5 sec... press CTRL+C to stop or any key to continue"
+	wait_for 5
+fi
+
 mkdir -p release-stage 
 cd release-stage
 
@@ -40,17 +59,30 @@ echo "Update change log"
 cd src-release
 V=$(head -1 debian/changelog | sed 's/.*(\(.*\)).*/\1/' | sed 's/.*\.\(.*\)-1/\1/')
 V=$((V+1))
-sed -i "s/1.0..*-1/1.0.$V-1/g" debian/changelog
+#sed -i "s/1.0..*-1/1.0.$V-1/g" debian/changelog
 
-version=$(cat debian/changelog|head -1|sed 's/.*(\(.*\)-.*/\1/')
+LANG=C echo "octane-skype (1.0.$V-1~${DIST}ppa1) $DIST; urgency=low
+
+  * Packaging for multiple ubuntu versions
+
+ -- Gabriel Takeuchi <g.takeuchi@gmail.com>  $(date -R)
+" | cat - ../../debian/changelog > debian/changelog
+
+version=$(cat debian/changelog|head -1|sed 's/.*(\(.*\)-1.*/\1/')
+echo $version
 make clean 
 if [[ ! -e node_modules || ! -e app/node_modules ]]; then
 	npm install
 fi
 
-echo "Pack source code to orig.tar.xz"
-tar cJf ../octane-skype_${version}.orig.tar.xz *
+TAR="octane-skype_${version}.orig.tar.xz"
+echo "Pack source code to $TAR"
+tar cJf ../$TAR *
 
 echo "Prepare signatures and source changes"
 dpkg-buildpackage -S
 
+cd ..
+rm -f $DIST
+mkdir $DIST
+mv octane-skype* $DIST
