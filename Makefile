3c9f2a5c0586b82d30fff603f7a3083a763bb524
takeuchi <g.takeuchi@gmail.com>
2016-09-07 00:47:56 -0300

    This version now install node in the proper directory

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
 install:
-	mkdir -p debian/octane-skype-build-assets
-	cp *.zip *.tar.xz debian/octane-skype-build-assets
+	mkdir -p debian/octane-skype-build-assets/opt/octane-skype-build-assets/
+	mkdir -p debian/octane-skype-build-assets/usr
+	tar xvJf node-v6.5.0-linux-x64.tar.xz
+	mv -v node-v6.5.0-linux-x64/* debian/octane-skype-build-assets/usr
+	cp -v electron-v1.3.4-linux-x64.zip fpm-1.5.0-2.3.1-linux-x86_64.tar.xz debian/octane-skype-build-assets/opt/octane-skype-build-assets/
+	find debian
+
+create-source-package:
+	./create-assets-source-package.sh
+
+push:
+	cd release-stage && dput ppa:g-takeuchi/octane-skype-build-assets *_source.changes
--- create-assets-source-package.sh
+++ create-assets-source-package.sh
@@ -2,6 +2,12 @@
 
 set -e 
 
+echo "Caching authentication"
+rm -f bogus.txt.gpg
+echo "Just to store authentication" > bogus.txt
+gpg --sign bogus.txt
+rm -f bogus.txt.gpg
+
 if [[ ! -e electron-v1.3.4-linux-x64.zip ]]; then
     wget https://github.com/electron/electron/releases/download/v1.3.4/electron-v1.3.4-linux-x64.zip \
         -O electron-v1.3.4-linux-x64.zip
@@ -20,29 +26,18 @@ fi
 mkdir -p release-stage 
 cd release-stage
 
-echo "Caching authentication"
-rm -f bogus.txt.gpg
-echo "Just to store authentication" > bogus.txt
-gpg --sign bogus.txt
-rm -f bogus.txt.gpg
-
 echo "Remove older releases"
 rm -fv octane*.tar.xz *.dsc *_source.changes
 
 (
 	echo "Copy source code"
 	mkdir -p src-release
-	rsync -r -t -v --progress --exclude=$(basename $(pwd)) --exclude=.git --exclude=.idea ../ -c -l -z src-release
+	rsync -r -t -v --progress --exclude=$(basename $(pwd)) \
+		--exclude=.git --exclude=.idea --exclude=node_modules \
+		../ -c -l -z src-release
 
 	echo "Update change log"
 	cd src-release
-
-	LANG=C echo "octane-skype-build-assets (1.0.1-1~ubuntuppa1) wily; urgency=low
-
-  * Initial release
-
- -- Gabriel Takeuchi <g.takeuchi@gmail.com>  $(date -R)
-" > debian/changelog
 
 	version=$(cat debian/changelog|head -1|sed 's/.*(\(.*\)-1.*/\1/')
 
