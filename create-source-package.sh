0d7bfb52df80fe6233c3d0a96589fa65ffa8c1b2
takeuchi <g.takeuchi@gmail.com>
2016-10-01 16:11:46 -0300

    Improving release script

--- create-source-package.sh
+++ create-source-package.sh
@@ -2,6 +2,49 @@
 
 set -e 
 
+CHANGES=
+
+echo "Type the changes introduced by this release, one by line, finish with CTRL+D"
+echo
+set -f
+while read -p'* ' line; do
+    CHANGES="$CHANGES  
+  * $line"
+done
+set +f
+
+function build_source()
+{
+    cd src-release
+
+    SERIES=$1
+    shift
+
+    V=$(head -1 debian/changelog | sed 's/.*(\(.*\)).*/\1/' | sed 's/.*\.\(.*\)-1.*/\1/')
+    V=$((V+1))
+
+    LANG=C echo "octane-skype (1.0.$V-1ubuntu1~$SERIES) $SERIES; urgency=low
+$CHANGES
+
+ -- Gabriel Takeuchi <g.takeuchi@gmail.com>  $(date -R)
+" | cat - ../../debian/changelog > debian/changelog
+
+    version=$(cat debian/changelog|head -1|sed 's/.*(\(.*\)-1.*/\1/')
+    echo $version
+    make clean 
+
+    TAR="octane-skype_${version}.orig.tar.xz"
+    echo "Pack source code to $TAR"
+    tar cJf ../$TAR *
+
+    echo "Prepare signatures and source changes"
+    dpkg-buildpackage -S -d
+    cd ..
+    mkdir -p $SERIES
+    mv octane*.tar.xz *.dsc *_source.changes $SERIES
+}
+
+
 mkdir -p release-stage 
 cd release-stage
 
@@ -12,31 +55,11 @@ gpg --sign bogus.txt
 rm -f bogus.txt.gpg
 
 echo "Remove older releases"
-rm -fv octane*.tar.xz *.dsc *_source.changes
+rm -rfv octane*.tar.xz *.dsc *_source.changes wily xenial
 
 echo "Copy source code"
 mkdir -p src-release
 rsync -r -t -v --progress --exclude=$(basename $(pwd)) --exclude=.git --exclude=.idea --exclude="*.xz" --exclude="*zip" --exclude=node_modules ../ -c -l -z src-release
 
-echo "Update change log"
-cd src-release
-#V=$(head -1 debian/changelog | sed 's/.*(\(.*\)).*/\1/' | sed 's/.*\.\(.*\)-1/\1/')
-#V=$((V+1))
-
-#LANG=C echo "octane-skype (1.0.$V-1) wily; urgency=low
-
-#  * Testing with separate node modules
-#
-# -- Gabriel Takeuchi <g.takeuchi@gmail.com>  $(date -R)
-#" | cat - ../../debian/changelog > debian/changelog
-
-version=$(cat debian/changelog|head -1|sed 's/.*(\(.*\)-1.*/\1/')
-echo $version
-make clean 
-
-TAR="octane-skype_${version}.orig.tar.xz"
-echo "Pack source code to $TAR"
-tar cJf ../$TAR *
-
-echo "Prepare signatures and source changes"
-dpkg-buildpackage -S -d
+build_source wily
+build_source xenial
